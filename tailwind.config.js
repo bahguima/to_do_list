cc18b35ed578f472c3685fb43b345238425fbe29
Bárbara Stanigher <barbaraguieiro@gmail.com>
2024-06-02 11:07:43 -0300

    initial commits conf tailwind and styles

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // base
+        gray700: "#0D0D0D",
+        gray600: "#1A1A1A",
+        gray500: "#262626",
+        gray400: "#333333",
+        gray300: "#808080",
+        gray200: "#D9D9D9",
+        gray100: "#F2F2F2",
+
+        //  produto
+        blueDark: "#1E6F9F",
+        blue: "#4EA8DE",
+        purpleDark: "#5E60CE",
+        purple: "#8284FA",
+
+        // feedback
+        danger: "#E25858",
+      },
+    },
+  },
+
+  plugins: [],
+};
